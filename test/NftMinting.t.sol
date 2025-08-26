97aca43062da9cc2278baf50614ad47e8d2139d4
jhavivek108 <jhavivek2404@gmail.com>
2025-08-26 17:38:49 +0000

    added script and setup function in test file

--- test/NftMinting.t.sol
+++ test/NftMinting.t.sol
@@ -7,7 +7,16 @@ import {NftMinting} from "../src/NftMinting.sol";
 contract NftMintingTest is Test {
     NftMinting public nftMinting;
 
-    function setUp() public {}
+    function setUp() public {
+        bytes32 merkleRoot;
+        string[] memory cmds= new string[](2);
+        cmds[0]= "node";
+        cmds[1]= "script/generateMerkleRoot";
+        
+        bytes memory data= vm.ffi(cmds);
+        merkleRoot= abi.decode(data,(bytes32));
+        console.logBytes32(merkleRoot);
+    }
 
     function testIncrement() public {}
 }
