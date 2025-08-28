6853bb1191a7c537edfc5b6da8c0822e2ecab96f
jhavivek108 <jhavivek2404@gmail.com>
2025-08-28 17:12:39 +0000

    added test for set cid and toggle state functions and resolved previous issues

--- test/NftMinting.t.sol
+++ test/NftMinting.t.sol
@@ -37,8 +37,8 @@ contract NftMintingTest is Test {
         string[] memory cids = new string[](2);
         cids[0] = "cid1";
         cids[1] = "cid2";
-        vm.prank(alice, alice);
 
+        vm.prank(alice, alice);
         nftMinting.publicSaleMint{value: 0.02 ether}(nftAmount, cids);
 
         assertEq(nftMinting.ownerOf(0), alice, "Owner of token 1 mismatch");
@@ -67,4 +67,41 @@ contract NftMintingTest is Test {
         uint256 postBalanceOwner = address(owner).balance;
         assertEq(postBalanceOwner - preBalanceOwner, 0.02 ether, "Balance Mismatch");
     }
+
+    function test_setCid() public {
+        nftMinting.togglePublicSale();
+        vm.stopPrank();
+
+        uint256 nftAmount = 1;
+        string[] memory cids = new string[](1);
+        cids[0] = "cid1";
+
+        vm.prank(alice, alice);
+        nftMinting.publicSaleMint{value: 0.01 ether}(nftAmount, cids);
+        assertEq(nftMinting.tokenCids(0), "cid1");
+
+        vm.prank(alice);
+        nftMinting.setCid(0, "cid0");
+        assertEq(nftMinting.tokenCids(0), "cid0");
+
+        vm.prank(owner);
+        vm.expectRevert("Owner Mismatch");
+        nftMinting.setCid(0, "cid2");
+    }
+
+    function test_toggleState() public {
+        assert(nftMinting.isPaused()==false);
+        assert(nftMinting.isPreSaleActive()==false);
+        assert(nftMinting.isPublicSaleActive()==false);
+
+        nftMinting.togglePause();
+        nftMinting.togglePreSale();
+        nftMinting.togglePublicSale();
+
+        assert(nftMinting.isPaused()==true);
+        assert(nftMinting.isPreSaleActive()==true);
+        assert(nftMinting.isPublicSaleActive()==true);
+    }
+
+
 }
